182ac7155383fadd8a64c2fa4651c75a0f7c8012
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-07-05 11:09:08 +0700

    ^ show online users

--- resources/js/components/Chat/Users.vue
+++ resources/js/components/Chat/Users.vue
@@ -1,11 +1,38 @@
 <template>
 	<div class="users">
-		<div class="users__header">1 user online</div>
-		<div class="users__user">
-			<a href="#">azharimm</a>
+		<div class="users__header">{{users.length}} {{pluralize('user', users.length)}} online</div>
+		<div class="users__user" v-for="user in users">
+			<a href="#">{{user.name}}</a>
 		</div>
 	</div>
 </template>
+
+<script>
+import pluralize from 'pluralize'
+import Bus from '../../bus'
+
+export default {
+	data() {
+		return {
+			users: []
+		}
+	},
+	mounted() {
+		Bus.$on('users.here', (users) => {
+			this.users = users;
+		})
+		.$on('users.joined',  (user) => {
+			this.users.unshift(user)
+		})
+		.$on('users.left', (user) => {
+			this.users = this.users.filter(u => u.id !== user.id)
+		})
+	},
+	methods: {
+		pluralize: pluralize
+	}
+}
+</script>
 
 <style lang="scss">
 	.users {
